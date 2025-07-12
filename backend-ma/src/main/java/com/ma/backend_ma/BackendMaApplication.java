ac0c3c4e2cb525f2dcc0bfd02ef2eb0d1676cd43
Zeka5 <zekovicognjen753@gmail.com>
2025-07-12 18:40:37 +0200

    added intial apps

---
+++ backend-ma/src/main/java/com/ma/backend_ma/BackendMaApplication.java
@@ -0,0 +1,13 @@
+package com.ma.backend_ma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMaApplication.class, args);
+	}
+
+}
---
+++ backend-ma/src/test/java/com/ma/backend_ma/BackendMaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma.backend_ma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
